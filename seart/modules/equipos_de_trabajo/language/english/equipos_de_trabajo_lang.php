d221f26d14c30ff1a684f51591c3d916c09c250d
betocuba77 <betocuba77@gmail.com>
2014-05-10 20:47:43 -0300

    Modificaciones Mensajes

--- seart/modules/equipos_de_trabajo/language/english/equipos_de_trabajo_lang.php
+++ seart/modules/equipos_de_trabajo/language/english/equipos_de_trabajo_lang.php
@@ -4,37 +4,37 @@ $lang['equipos_de_trabajo_manage']			= 'Gestión de Equipos de Trabajo';
 $lang['equipos_de_trabajo_edit']				= 'Editar';
 $lang['equipos_de_trabajo_true']				= 'True';
 $lang['equipos_de_trabajo_false']				= 'False';
-$lang['equipos_de_trabajo_create']			= 'Crear';
+$lang['equipos_de_trabajo_create']				= 'Crear';
 $lang['equipos_de_trabajo_list']				= 'Listar';
-$lang['equipos_de_trabajo_new']				= 'Nuevo';
-$lang['equipos_de_trabajo_edit_text']			= 'Edit this to suit your needs';
-$lang['equipos_de_trabajo_no_records']		= 'There aren\'t any equipos_de_trabajo in the system.';
-$lang['equipos_de_trabajo_create_new']		= 'Crear un nuevo Equipos de Trabajo.';
-$lang['equipos_de_trabajo_create_success']	= 'Equipo de Trabajo creado satisfactoriamente.';
-$lang['equipos_de_trabajo_create_failure']	= 'There was a problem creating the equipos_de_trabajo: ';
-$lang['equipos_de_trabajo_create_new_button']	= 'Crear nuevo Equipo de Trabajo';
-$lang['equipos_de_trabajo_invalid_id']		= 'Invalid Equipos de Trabajo ID.';
-$lang['equipos_de_trabajo_edit_success']		= 'Equipos de Trabajo guardado satisfactoriamente.';
-$lang['equipos_de_trabajo_edit_failure']		= 'There was a problem saving the equipos_de_trabajo: ';
-$lang['equipos_de_trabajo_delete_success']	= 'record(s) successfully deleted.';
-$lang['equipos_de_trabajo_delete_failure']	= 'We could not delete the record: ';
-$lang['equipos_de_trabajo_delete_error']		= 'You have not selected any records to delete.';
+$lang['equipos_de_trabajo_new']					= 'Nuevo';
+$lang['equipos_de_trabajo_edit_text']			= 'Editar según su necesidad.';
+$lang['equipos_de_trabajo_no_records']		= 'No hay equipos de trabajo en el sistema.';
+$lang['equipos_de_trabajo_create_new']		= 'Crear un nuevo equipo de trabajo.';
+$lang['equipos_de_trabajo_create_success']	= 'Equipo de Trabajo creado correctamente.';
+$lang['equipos_de_trabajo_create_failure']	= 'Hubo un problema al crear el equipo de trabajo: ';
+$lang['equipos_de_trabajo_create_new_button']	= 'Crear nuevo equipo de trabajo';
+$lang['equipos_de_trabajo_invalid_id']		= 'ID de equipo de trabajo no válido.';
+$lang['equipos_de_trabajo_edit_success']		= 'Equipo de Trabajo guardado correctamente.';
+$lang['equipos_de_trabajo_edit_failure']		= 'Hubo un problema al guardar el equipo de trabajo: ';
+$lang['equipos_de_trabajo_delete_success']	= 'Registro(s) borrado correctamente.';
+$lang['equipos_de_trabajo_delete_failure']	= 'No se puede borrar el registro: ';
+$lang['equipos_de_trabajo_delete_error']		= 'No tienes seleccionado ningún registro para borrar.';
 $lang['equipos_de_trabajo_actions']			= 'Acciones';
 $lang['equipos_de_trabajo_cancel']			= 'Cancelar';
-$lang['equipos_de_trabajo_delete_record']		= 'Borrar este Equipos de Trabajo';
-$lang['equipos_de_trabajo_delete_confirm']	= 'Are you sure you want to delete this equipos_de_trabajo?';
-$lang['equipos_de_trabajo_edit_heading']		= 'Editar Equipos de Trabajo';
+$lang['equipos_de_trabajo_delete_record']		= 'Borrar este equipos de trabajo';
+$lang['equipos_de_trabajo_delete_confirm']	= '¿Estás seguro de borrar este equipo de trabajo?';
+$lang['equipos_de_trabajo_edit_heading']		= 'Editar equipos de trabajo';
 
 // Create/Edit Buttons
-$lang['equipos_de_trabajo_action_edit']		= 'Guardar Equipos de Trabajo';
-$lang['equipos_de_trabajo_action_create']		= 'Crear Equipos de Trabajo';
+$lang['equipos_de_trabajo_action_edit']		= 'Guardar equipos de trabajo';
+$lang['equipos_de_trabajo_action_create']		= 'Crear equipos de trabajo';
 
 // Activities
-$lang['equipos_de_trabajo_act_create_record']	= 'Created record with ID';
-$lang['equipos_de_trabajo_act_edit_record']	= 'Updated record with ID';
-$lang['equipos_de_trabajo_act_delete_record']	= 'Deleted record with ID';
+$lang['equipos_de_trabajo_act_create_record']	= 'Registro creado con ID';
+$lang['equipos_de_trabajo_act_edit_record']	= 'Registro actualizado con ID';
+$lang['equipos_de_trabajo_act_delete_record']	= 'Registro borrado con ID';
 
 // Column Headings
-$lang['equipos_de_trabajo_column_created']	= 'Created';
-$lang['equipos_de_trabajo_column_deleted']	= 'Deleted';
-$lang['equipos_de_trabajo_column_modified']	= 'Modified';
+$lang['equipos_de_trabajo_column_created']	= 'Creado';
+$lang['equipos_de_trabajo_column_deleted']	= 'Borrado';
+$lang['equipos_de_trabajo_column_modified']	= 'Modificado';
--- seart/modules/preguntas/language/spanish_am/preguntas_lang.php
+++ seart/modules/preguntas/language/spanish_am/preguntas_lang.php
@@ -10,28 +10,28 @@ $lang['preguntas_new']				= 'Nueva';
 $lang['preguntas_edit_text']		= 'Edite según sus necesidades.';
 $lang['preguntas_no_records']		= 'No hay ninguna pregunta en el sistema.';
 $lang['preguntas_create_new']		= 'Crear una nueva pregunta.';
-$lang['preguntas_create_success']	= 'Pregunta creada satisfactoriamente.';
+$lang['preguntas_create_success']	= 'Pregunta creada correctamente.';
 $lang['preguntas_create_failure']	= 'Hubo un problema al crear la pregunta: ';
 $lang['preguntas_create_new_button']	= 'Crear nueva pregunta';
 $lang['preguntas_invalid_id']		= 'ID de pregunta no válido.';
-$lang['preguntas_edit_success']		= 'Preguntas guardadas satisfactoriamente.';
+$lang['preguntas_edit_success']		= 'Pregunta guardada correctamente.';
 $lang['preguntas_edit_failure']		= 'Hubo un problema al guardar la pregunta: ';
-$lang['preguntas_delete_success']	= 'Registro(s) borrados satisfactoriamente.';
+$lang['preguntas_delete_success']	= 'Registro(s) borrados corractamente.';
 $lang['preguntas_delete_failure']	= 'No se puede borrar el registro: ';
 $lang['preguntas_delete_error']		= 'No tienes seleccionado ningún registro para borrar.';
 $lang['preguntas_actions']			= 'Acciones';
 $lang['preguntas_cancel']			= 'Cancelar';
 $lang['preguntas_delete_record']		= 'Borrar';
 $lang['preguntas_delete_confirm']	= '¿Estás seguro de borrar la(s) pregunta(s)?';
-$lang['preguntas_edit_heading']		= 'Editar Preguntas';
+$lang['preguntas_edit_heading']		= 'Editar preguntas';
 
 // Create/Edit Buttons
-$lang['preguntas_action_edit']		= 'Guardar';
+$lang['preguntas_action_edit']			= 'Guardar';
 $lang['preguntas_action_create']		= 'Agregar';
 
 // Activities
 $lang['preguntas_act_create_record']	= 'Registro creado con ID';
-$lang['preguntas_act_edit_record']	= 'Registro actualizado con ID';
+$lang['preguntas_act_edit_record']		= 'Registro actualizado con ID';
 $lang['preguntas_act_delete_record']	= 'Registro borrado ID';
 
 // Column Headings
